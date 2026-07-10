d9992a755e869dcf200816546757a3dda8de1601
tkatochin <t.katochin@gmail.com>
2026-07-11 01:35:34 +0900

    main 第4章追加とスタークコード整理

--- scripts/build_novel.rb
+++ scripts/build_novel.rb
@@ -8,12 +8,20 @@ output_path = File.join(root, '小説版_本文.md')
 chapters = Dir.glob(File.join(chapters_dir, '[0-9][0-9]_*.md')).sort
 abort('No chapter files found in novel_chapters/') if chapters.empty?
 
-body = chapters.map { |path| File.read(path).rstrip }.join("\n\n")
+part_titles = {
+  '01' => '## 第一部 父の残した部屋',
+  '04' => '## 第二部 ひとりでは続かない'
+}
+
+body_sections = chapters.flat_map do |path|
+  chapter_number = File.basename(path)[/^\d{2}/]
+  [part_titles[chapter_number], File.read(path).rstrip].compact
+end
+
+body = body_sections.join("\n\n")
 
 output = <<~MARKDOWN.rstrip
   # ちいかわ アイアンアーマーズ 小説版
-
-  ## 第一部 父の残した部屋
 
   #{body}
 MARKDOWN
